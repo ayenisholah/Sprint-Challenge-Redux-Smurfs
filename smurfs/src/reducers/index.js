caaf2e4709b908505e54b04e77ae572d6557d0df
Shola Ayeni <ayenisholah@yahoo.com>
2019-06-28 09:27:41 +0100

    smurfsReducer function

--- smurfs/src/reducers/index.js
+++ smurfs/src/reducers/index.js
@@ -1,23 +1,26 @@
-/*
-  Be sure to import in all of the action types from `../actions`
-*/
+import {
+  GETTING_SMURFS,
+  GET_SMURF_SUCCESS,
+  GET_SMURF_FAILURE,
+  ADDING_SMURF,
+  ADD_SMURF_SUCCESS,
+  ADD_SMURF_FAILURE,
+  DELETING_SMURF,
+  DELETE_SMURF_SUCCESS,
+  DELETE_SMURF_FAILURE
+} from '../actions/actionTypes';
 
-/*
- Your initial/default state for this project could *Although does not have to* look a lot like this
- {
-   smurfs: [],
-   fetchingSmurfs: false
-   addingSmurf: false
-   updatingSmurf: false
-   deletingSmurf: false
-   error: null
- }
-*/
 
-/*
-  You'll only need one smurf reducer for this project.
-  Feel free to export it as a default and import as rootReducer. 
-  This will guard your namespacing issues.
-  There is no need for 'combineReducers' in this project.
-  Components can then read your store as, `state` and not `state.fooReducer`.
-*/
+export const smurfsReducer = (sliceOfState = [], action) => {
+  switch (action.type) {
+    case GET_SMURF_SUCCESS:
+      return action.payload;
+    case ADD_SMURF_SUCCESS:
+      return action.payload;
+    case DELETE_SMURF_SUCCESS:
+      return action.payload;
+    default:
+      return sliceOfState;
+  }
+}
+
